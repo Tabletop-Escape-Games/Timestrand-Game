bb24ee827b5b469b58eec42196bb2c2a06b559e6
Matthijs <m.zwaan@student.fontys.nl>
2024-06-15 14:08:07 +0200

    Removed classes used for dragging
    Game will not implement any dragging actions, removed classes created for this.

--- Assets/Scripts/Layer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Layer // Een klasse voor het beheren van de layers, zodat waarden niet in de controller hoeven te worden aangepast
-{
-    public static readonly int Default = 0; // De standaard layer
-    public static readonly int Dragging = 6; // De layer voor het slepen
-}
