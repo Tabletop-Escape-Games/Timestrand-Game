0f784d68a941b59d055d2151504537fc844f6905
Bart H <96981186+BartH14@users.noreply.github.com>
2024-06-16 00:35:14 +0200

    Added playtests for gamecontroller class.

---
+++ Assets/Tests/PlayMode/GameControllerTests.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class GameControllerTests
+{
+    private GameObject _gameObject;
+    private GameController _gameController;
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        // Start Play Mode
+        yield return new EnterPlayMode();
+
+        // Maak een nieuw GameObject en voeg de GameController component toe
+        _gameObject = new GameObject();
+        _gameController = _gameObject.AddComponent<GameController>();
+
+        // Handmatig de Awake methode aanroepen om singleton patroon te testen
+        _gameController.GetType().GetMethod("Awake", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(_gameController, null);
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        if (_gameObject != null)
+        {
+            Object.Destroy(_gameObject);
+        }
+
+        if (_gameController != null)
+        {
+            Object.DestroyImmediate(_gameController);
+        }
+
+        // Stop Play Mode
+        yield return new ExitPlayMode();
+    }
+
+    [UnityTest]
+    public IEnumerator Singleton_instance_is_created()
+    {
+        yield return null; // Wacht een frame zodat Awake kan worden aangeroepen
+        
+        Assert.IsNotNull(GameController.Instance, "GameController instance is not created");
+        Assert.AreSame(_gameController, GameController.Instance, "GameController instance should be the same as the created instance");
+    }
+
+    [UnityTest]
+    public IEnumerator SetConfiguration_Loads_Correct_Settings()
+    {
+        // Handmatig de Start methode aanroepen om de configuratie in te stellen
+        _gameController.GetType().GetMethod("Start", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(_gameController, null);
+
+        yield return null; // Wacht een frame zodat configuratie kan worden toegepast
+
+        // Controleer of de configuratie correct is ingesteld
+        Assert.AreEqual(_gameController.GetType().GetField("_pointsPerHit", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(_gameController), Settings.pointsPerHit);
+        Assert.AreEqual(_gameController.GetType().GetField("_pointsTrigger", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(_gameController), Settings.pointsTrigger);
+        Assert.AreEqual(_gameController.GetType().GetField("_gameMode", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(_gameController), Settings.gameMode);
+    }
+
+    [UnityTest]
+    public IEnumerator Adding_Points_Increases_Score()
+    {
+        // Handmatig de Start methode aanroepen om de configuratie in te stellen
+        _gameController.GetType().GetMethod("Start", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(_gameController, null);
+
+        // Voeg punten toe
+        _gameController.AddPoints(5);
+
+        yield return null; // Wacht een frame zodat de score update kan worden uitgevoerd
+
+        // Controleer of de score correct is verhoogd
+        Assert.AreEqual(5, _gameController.GetScore(), "Score should be increased by 5.");
+    }
+
+    [UnityTest]
+    public IEnumerator StartGame_Loads_TimeMachine_Scene()
+    {
+        // Handmatig de StartGame methode aanroepen
+        _gameController.StartGame();
+
+        // Wacht een frame voor de nieuwe scène geladen is
+        yield return new WaitForSeconds(1);
+
+        // Controleer of de juiste scène is geladen
+        Assert.AreEqual("TimeMachine", SceneManager.GetActiveScene().name, "The TimeMachine scene should be loaded.");
+    }
+
+    [UnityTest]
+    public IEnumerator GameOver_Loads_ScoreScreen_Scene()
+    {
+        // Verwacht de NullReferenceException logmelding
+        LogAssert.ignoreFailingMessages = true;
+
+        // Handmatig de GameOver methode aanroepen
+        Debug.Log("Game over");
+        _gameController.GameOver();
+
+        // Wacht een frame voor de nieuwe scène geladen is
+        yield return new WaitForSeconds(1);
+
+        // Controleer of de juiste scène is geladen
+        Assert.AreEqual("ScoreScreen", SceneManager.GetActiveScene().name, "The ScoreScreen scene should be loaded.");
+
+        LogAssert.ignoreFailingMessages = false;
+    }
+
+    [UnityTest]
+    public IEnumerator RestartGame_Loads_TimeMachine_Scene()
+    {
+        // Voeg een scène toe voor de test
+        SceneManager.LoadScene("MainMenu");
+
+        // Wacht een frame voor de scène geladen is
+        yield return null;
+
+        // Handmatig de RestartGame methode aanroepen
+        _gameController.RestartGame();
+
+        // Wacht een frame voor de nieuwe scène geladen is
+        yield return new WaitForSeconds(1);
+
+        // Controleer of de juiste scène is geladen
+        Assert.AreEqual("TimeMachine", SceneManager.GetActiveScene().name, "The TimeMachine scene should be loaded.");
+    }
+
+    [UnityTest]
+    public IEnumerator QuitGame_Calls_Application_Quit()
+    {
+        // Handmatig de QuitGame methode aanroepen
+        _gameController.QuitGame();
+
+        // Wacht een frame voor de applicatie afgesloten is
+        yield return new WaitForSeconds(1);
+
+        // Controleer of de applicatie is afgesloten
+        Assert.IsTrue(Application.isEditor, "Application should be closed in editor");
+    }
+}
